61c97949d79378e7dca91d76f530cb2ffd762f0d
Eren Nevin <eren.m.nevin@gmail.com>
2023-09-14 14:08:48 +0300

    Added profiling tools

--- aws-rotation/src/routes/api/ip_change.ts
+++ aws-rotation/src/routes/api/ip_change.ts
@@ -7,10 +7,13 @@ import { logger } from "./utils";
 
 // TODO: Add error handling
 export async function rotateInstance(mInstance: Instance, regionRequesthandler: RegionRequestHandler, cronHandler: CronHandler) {
+    console.time("Rotate Instance");
     // NOTE: Does this need to be here?
-    cronHandler.attachEmptyCronToInstancesIfNeeded(regionRequesthandler.instances)
+    // cronHandler.attachEmptyCronToInstancesIfNeeded(regionRequesthandler.instances)
+
 
     // INFO: Information gathering and validation
+    console.time("Info Gathering");
     const instanceId: string = mInstance.arn ?? '';
     if (instanceId === '') { logger.error(`RotateInstance: ${mInstance.name} instance id empty`); return false };
     if (!mInstance.name) { logger.error(`RotateInstance: ${mInstance.name} instance name empty`); return false };
@@ -23,6 +26,7 @@ export async function rotateInstance(mInstance: Instance, regionRequesthandler: 
     let instance = regionRequesthandler.instances.find((i) => i.arn === mInstance.arn);
     if (!instance) { logger.error(`RotateInstance: ${mInstance.name} not found`); return false };
     if (!instance.name) { logger.error(`RotateInstance: ${instance} doesnt have name`); return false };
+
 
     const currentStaticIpAddress = instance.publicIpAddress;
     const currentStaticIp = regionRequesthandler.static_ips.find((ip) => ip.ipAddress === currentStaticIpAddress);
@@ -40,24 +44,13 @@ export async function rotateInstance(mInstance: Instance, regionRequesthandler: 
 
     let currentDomain: Domain | undefined
 
-    console.log(regionRequesthandler.domains);
-    console.log(regionRequesthandler.domains);
-    console.log(regionRequesthandler.domains);
-    console.log(regionRequesthandler.domains);
     for (let domain of regionRequesthandler.domains) {
         const addressDomainEntries = domain.domainEntries?.filter((de) => de.type === "A");
-        console.warn(addressDomainEntries);
-        console.error(addressDomainEntries);
-        console.log(addressDomainEntries);
-        console.log(addressDomainEntries);
         if (addressDomainEntries) {
             if (addressDomainEntries.find((de) => de.name === constantDomainName)) {
                 currentDomain = domain
                 break;
             }
-            //     if (addressDomainEntries.find((de) => de.target === currentStaticIpAddress)) {
-            //         currentDomains = [...currentDomains, domain];
-            //     }
         }
     }
 
@@ -65,6 +58,8 @@ export async function rotateInstance(mInstance: Instance, regionRequesthandler: 
     logger.info(`RotateInstance: ${instance.name} connected domain ${currentDomain}`);
     if (!currentStaticIp || !currentStaticIpName || !currentDomain) { logger.error(``); return false };
 
+    console.timeEnd("Info Gathering");
+    console.time("Detaching Current Static Ip");
     // INFO: Operation 1: detach current static ip
     let res = await regionRequesthandler.detachStaticIpFromInstance(instance.name);
     if (res) {
@@ -73,8 +68,10 @@ export async function rotateInstance(mInstance: Instance, regionRequesthandler: 
         logger.error(`RotateInstance: ${instance.name} Failed to detach static ip from instance`);
         return false;
     }
+    console.timeEnd("Detaching Current Static Ip");
 
     // INFO: Operation 2: release detached static ip
+    console.time("Releasing Previous Static Ip");
     res = await regionRequesthandler.releaseStaticIp(currentStaticIpName);
     if (res) {
         logger.info(`RotateInstance: ${instance.name} Released Previous IP: ${currentStaticIpName}`);
@@ -82,8 +79,11 @@ export async function rotateInstance(mInstance: Instance, regionRequesthandler: 
         logger.error(`RotateInstance: ${instance.name} Failed to release previous static ip ${currentStaticIpName}`);
         return false;
     }
+    console.timeEnd("Releasing Previous Static Ip");
 
     // INFO: Operation 3: Allocate New IP:
+
+    console.time("Allocating New Static Ip");
     let newIpName = generateStaticIpName(Math.floor(Math.random() * 100000000));
 
     res = await regionRequesthandler.allocateStaticIp(newIpName);
@@ -93,12 +93,10 @@ export async function rotateInstance(mInstance: Instance, regionRequesthandler: 
         logger.error(`RotateInstance: ${instance.name} Failed to allocate new static ip ${newIpName}}`);
         return false;
     }
-
-
-
-
+    console.timeEnd("Allocating New Static Ip");
 
     // INFO: Operation 4: Attach allocated new ip:
+    console.time("Attaching New Static Ip");
     res = await regionRequesthandler.attachStaticIpToInstance(instance.name, newIpName);
     if (res) {
         logger.info(`RotateInstance: ${instance.name} Attached New IP: ${newIpName} to instance`);
@@ -106,8 +104,10 @@ export async function rotateInstance(mInstance: Instance, regionRequesthandler: 
         logger.error(`RotateInstance: ${instance.name} Failed to attach new static ip ${newIpName} to instance`);
         return false;
     }
+    console.timeEnd("Attaching New Static Ip");
 
     // INFO: Operation 5: Find ip address of new static ip
+    console.time("Finding New Static Ip Address");
     const refreshedStaticIPs = await regionRequesthandler.refreshStaticIps()
     if (!refreshedStaticIPs) {
         logger.error(`RotateInstance: ${instance.name} Failed to refresh static ips`);
@@ -115,42 +115,21 @@ export async function rotateInstance(mInstance: Instance, regionRequesthandler: 
     }
     let newStaticIpAddress = regionRequesthandler.static_ips.find((ip) => ip.name === newIpName)?.ipAddress;
     if (!newStaticIpAddress) { logger.error(`RotateInstance: ${instance.name} Failed to find new static ip address`); return false }
-    console.warn(newStaticIpAddress);
     // await wait(1000);
+    console.timeEnd("Finding New Static Ip Address");
 
     // INFO: Operation 6: Clear previous domain entries pointing to previous static ip
+    console.time("Clearing Previous Domain Entries");
     res = await regionRequesthandler.clearDomainIps(currentDomain.name!,
         [currentStaticIp.ipAddress!]);
     if (!res) { logger.error(`RotateInstance: ${instance.name} Failed to clear previous domain entries pointing to previous static ip`); return false };
 
     logger.info(`RotateInstance: ${instance.name} Cleared Previous domains entries pointing to previous static ip`);
+    console.timeEnd("Clearing Previous Domain Entries");
 
-
-    // await wait(1000);
-
-
-    // await wait(1000);
-
-
-    // await wait(1000);
-
-    // const newInstancesRefreshed = await regionRequesthandler.refreshInstances()
-    // if (newInstancesRefreshed) {
-    //     logger.info(`RotateInstance: ${instance.name} Refreshed Instances`);
-    // } else {
-    //     logger.error(`RotateInstance: ${instance.name} Failed to refresh instances`);
-    //     return false;
-    // }
-    //
-    // const myInstance = regionRequesthandler.instances.find((i) => i.name === instance?.name);
-    // if (myInstance) {
-    //     logger.info(`RotateInstance: ${instance.name} Found updated instance`);
-    // } else {
-    //     logger.error(`RotateInstance: ${instance.name} Failed to find updated instance`);
-    //     return false;
-    // }
 
     // INFO: Operation 7: Point domain to new static ip
+    console.time("Pointing Domain to New Static Ip");
     res = await regionRequesthandler.pointDomainToIp(currentDomain?.name!, constantDomainName, newStaticIpAddress);
     if (res) {
         logger.info(`RotateInstance: ${instance.name} Pointed domain ${currentDomain} to new ip ${newStaticIpAddress} by ${constantDomainName}`);
@@ -158,12 +137,14 @@ export async function rotateInstance(mInstance: Instance, regionRequesthandler: 
         logger.error(`RotateInstance: ${instance.name} Failed to point domain ${currentDomain} to new ip ${newStaticIpAddress} by ${constantDomainName}`);
         return false;
     }
+    console.timeEnd("Pointing Domain to New Static Ip");
 
     if (res) {
         logger.info(`RotateInstance: ${instance.name} Successfully rotated instance`);
     } else {
         logger.error(`RotateInstance: ${instance.name} Failed to rotate instance`);
     }
+    console.timeEnd("Rotate Instance");
     return res;
 }
 
